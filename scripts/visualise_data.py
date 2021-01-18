18a081c9112dd59ba3c792d1455e19d8bb8dfbf3
Martin Engelcke <31702388+martinengelcke@users.noreply.github.com>
2021-01-18 12:14:03 +0000

    Show ground truth masks if available when visualising data.

--- scripts/visualise_data.py
+++ scripts/visualise_data.py
@@ -11,6 +11,7 @@
 #
 # =========================== A2I Copyright Header ===========================
 
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import NoNorm
@@ -43,19 +44,47 @@ def main():
     # Get data loaders
     train_loader, _, _ = fet.load(cfg.data_config, cfg)
 
+    # Optimally distinct RGB colour palette (15 colours)
+    colours = json.load(open('utils/colour_palette15.json'))
+
     # Visualise
     for x in train_loader:
-        fig, axes = plt.subplots(1, cfg.batch_size, figsize=(20, 10))
+        fig, axes = plt.subplots(2, cfg.batch_size, figsize=(20,10))
 
-        img = x['input']
-        for b_idx in range(cfg.batch_size):
-            np_img = np.moveaxis(img.data.numpy()[b_idx], 0, -1)
-            if img.shape[1] == 1:
-                axes[b_idx].imshow(
-                    np_img[:, :, 0], norm=NoNorm(), cmap='gray')
-            elif img.shape[1] == 3:
-                axes[b_idx].imshow(np_img)
-            axes[b_idx].axis('off')
+        for f_idx, field in enumerate(['input', 'instances']):
+            for b_idx in range(cfg.batch_size):
+                axes[f_idx, b_idx].axis('off')
+
+            if field not in x:
+                continue
+            img = x[field]
+
+            # Colour instance masks
+            if field == 'instances':
+                img_list = []
+                for b_idx in range(img.shape[0]):
+                    instances = img[b_idx, :, :, :]
+                    img_r = torch.zeros_like(instances)
+                    img_g = torch.zeros_like(instances)
+                    img_b = torch.zeros_like(instances)
+                    ins_idx = 0
+                    for ins in range(instances.max().numpy()):
+                        ins_map = instances == ins + 1
+                        if ins_map.any():
+                            img_r[ins_map] = colours['palette'][ins_idx][0]
+                            img_g[ins_map] = colours['palette'][ins_idx][1]
+                            img_b[ins_map] = colours['palette'][ins_idx][2]
+                            ins_idx += 1
+                    img_list.append(torch.cat([img_r, img_g, img_b], dim=0))
+                img = torch.stack(img_list, dim=0)
+
+            for b_idx in range(cfg.batch_size):
+                np_img = np.moveaxis(img.data.numpy()[b_idx], 0, -1)
+                if img.shape[1] == 1:
+                    axes[f_idx, b_idx].imshow(
+                        np_img[:, :, 0], norm=NoNorm(), cmap='gray')
+                elif img.shape[1] == 3:
+                    axes[f_idx, b_idx].imshow(np_img)
 
         manager = plt.get_current_fig_manager()
         manager.resize(*manager.window.maxsize())
